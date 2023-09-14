870d77c5b99833350f7393e05814a901ace56d9f
klyshnikova <klyshniova@mail.ru>
2023-09-14 18:15:11 +0200

    провожу экперимент с js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-
-    let b = document.querySelectorAll('.btn')
-    b[0].onclick = function(){
-        b[0].innerText = 'Piska poluchilos'
-        }
-
+function myFuncHero(){
+    const valueBtnHero = document.querySelectorAll('.btn-hero')
+    if (valueBtnHero[0].onclick){
+        valueBtnHero[0].style.color = 'red'
+    }else{
+        valueBtnHero[0].style.color = 'white'
+    }
+}
+function myFunc(){
+    const valueBtn = document.querySelectorAll('.btn')
+    if (valueBtn[0].onclick){
+        valueBtn[0].style.color = 'red'
+    }else{
+        valueBtn[0].style.color = 'white'
+    }
+}
